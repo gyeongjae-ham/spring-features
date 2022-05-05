66fdcd55742ab2617dec0beef777f3af75c35955
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-05-05 15:09:16 +0900

    :sparkles: request mapping features
    header, params, consumes, produces

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -23,6 +23,7 @@ public class MappingController {
 
     /**
      * 편리한 축약 에노테이션(코드보기)
+     *
      * @GetMapping
      * @PostMapping
      * @PutMapping
@@ -46,6 +47,73 @@ public class MappingController {
     @GetMapping("/mapping/{userId}")
     public String mappingPath(@PathVariable String userId) {
         log.info("mappingPath userId={}", userId);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug"
+     * params={"mode=debug", "data=good"}
+     */
+    // params에 지정한 조건의 parameter가 없으면 호출 안됨(400 BAD_REQUEST)
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode",
+     * headers="!mode"
+     * headers="mode=debug"
+     * headers="mode!=debug"
+     */
+    // headers에 지정한 조건의 header 없이 요청하면 안됨
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content=Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    // request 오는 미디어 타입의 조건을 지정할 수 있다
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*"
+     * produces = "*\/*"
+     */
+    // 클라이언트 입장에서 받아들일 수 있는 Accept 타입과 내가 생산하는 타입이 일치할 경우에만 응답
+    @PostMapping(value = "/mapping-produces", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
         return "ok";
     }
 }
