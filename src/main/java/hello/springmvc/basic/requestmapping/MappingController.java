ac144650688b16177fcf723e5856a520e0628f98
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-05-05 15:10:32 +0900

    :recycle: request mapping media type consumes refactor
    String으로 지정하는 방식보다 더 나은 방식

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,6 +2,7 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -97,7 +98,7 @@ public class MappingController {
      * MediaType.APPLICATION_JSON_VALUE
      */
     // request 오는 미디어 타입의 조건을 지정할 수 있다
-    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String mappingConsumes() {
         log.info("mappingConsumes");
         return "ok";
