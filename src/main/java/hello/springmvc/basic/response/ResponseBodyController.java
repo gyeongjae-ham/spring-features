8a69c178040f8908702be1207b9c09bb62a66e95
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-05-11 17:19:11 +0900

    :sparkles: HTTP 응답 - response body string v1

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+}
