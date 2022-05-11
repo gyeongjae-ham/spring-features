64f0f3abbe4402a9309d97901b2db79ac64d4953
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-05-11 17:19:54 +0900

    :sparkles: response body string v2
    use ResponseEntity<>

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -1,6 +1,8 @@
 package hello.springmvc.basic.response;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,6 +17,11 @@ public class ResponseBodyController {
     @GetMapping("/response-body-string-v1")
     public void responseBodyV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() throws IOException {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
 }
